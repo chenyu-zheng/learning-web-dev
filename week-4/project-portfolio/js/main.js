1ab5ca85e5d9ac0ed1e977241982229e454efa94
chenyu-zheng <zcy9998@gmail.com>
2018-03-12 02:59:22 -0500

    Project: Portfolio
    Udacity web project 2

---
+++ week-4/project-portfolio/js/main.js
@@ -0,0 +1,53 @@
+const container = document.getElementById("hero-container");
+const images = document.getElementsByClassName("hero-img");
+
+let curr = 0;
+
+let intervalId;
+
+for (i = 0; i < images.length; i++) {
+    if (i !== curr) {
+        images[i].style.display = "none";
+    }
+}
+
+container.onmouseover = stop;
+container.onmouseout = play;
+
+play();
+
+
+
+function play() {
+    intervalId = setInterval(switchImg, 3000);
+}
+
+function stop() {
+    clearInterval(intervalId);
+}
+
+
+function switchImg() {
+    let next = curr + 1;
+    if (next >= images.length) {
+        next = 0;
+    }
+    let opa = 100;
+    let sInterId = setInterval(
+        function () {
+            opa -= 10;
+            images[curr].style.opacity = opa / 100;
+            images[next].style.opacity = 1 - opa / 100;
+            images[next].style.display = "block";
+            if (opa <= 0) {
+                clearInterval(sInterId);
+                images[curr].style.display = "none";
+                curr++
+                if (curr >= images.length) {
+                    curr = 0;
+                }
+            }
+        },
+        100
+    );
+}
