ea6299df3087c5386710cb39c19a3d89bc8e21dd
chenyu-zheng <zcy9998@gmail.com>
2018-03-14 08:59:19 -0500

    add exercise: pixel art maker

---
+++ week-6/project-pixel-art-maker/js/designs.js
@@ -0,0 +1,39 @@
+const canvas = document.getElementById("pixel-canvas");
+const colorPicker = document.getElementById("color-picker");
+const inputHeight = document.getElementById("input-height");
+const inputWidth = document.getElementById("input-width");
+const resetButton = document.getElementById("reset-button");
+
+canvas.addEventListener("click", updatePixel);
+resetButton.addEventListener("click", resetCanvas);
+
+resetCanvas();
+
+
+
+function resetCanvas () {
+
+    const height = parseInt(inputHeight.value);
+    const width = parseInt(inputWidth.value);
+
+    let tblHtml = "";
+
+    for (let i = 0; i < height; i++) {
+        tblHtml += `<tr y="${i}">`
+        for (let j = 0; j < width; j++) {
+            tblHtml += `<td x="${j}"></td>`
+        }
+        tblHtml += "</tr>"
+    }
+
+    canvas.innerHTML = tblHtml;
+}
+
+
+function updatePixel (evt) {
+    if (evt.target.tagName !== "TD") return;
+    const y = evt.target.parentNode.getAttribute("y");
+    const x = evt.target.getAttribute("x");
+    const td = canvas.querySelector(`tr[y="${y}"] td[x="${x}"]`);
+    td.style.backgroundColor = colorPicker.value;
+}
